9924f94d5e779185cbdbb3a1dc4a9eefb2d395eb
DESKTOP-INGTQEO\Lander <david.calderon.landeros@gmail.com>
2023-11-23 21:37:48 -0600

    example 2

--- examples.js
+++ examples.js
@@ -2,3 +2,37 @@
 function multiply(a, b){
     return a * b;
 }
+//Arrow function
+ multiply = (a,b) => a * b;
+
+//Example 2: Switch case
+function getPlanetName(id){
+    var name;
+    switch(id){
+        case 1:
+            name = 'Mercury'
+            break;
+        case 2:
+            name = 'Venus'
+            break;
+        case 3:
+            name = 'Earth'
+            break;
+        case 4:
+            name = 'Mars'
+            break;
+        case 5:
+            name = 'Jupiter'
+            break;
+        case 6:
+            name = 'Saturn'
+            break;
+        case 7:
+            name = 'Uranus'
+            break;
+        case 8:
+            name = 'Neptune' 
+            break;
+    }
+    return name;
+}
