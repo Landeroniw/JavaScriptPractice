2caeafbd87b14061234a0ed21b1abe95412bf752
DESKTOP-INGTQEO\Lander <david.calderon.landeros@gmail.com>
2023-11-23 22:33:54 -0600

    example 10

--- examples.js
+++ examples.js
@@ -121,3 +121,13 @@ class smallestIntegerFinder{
         return smallestNumber;
     }
 }
+
+//Example 10: Sum of factorial
+var sumation = function (num){
+    let sum = 0;
+    for(let i = 1; i <= num; i++){
+        sum = sum + i;
+    }
+    return sum;
+}
+
